9168d3e2695076cbff87a4078d19a0c21c248eda
alecg01 <112833836+alecg01@users.noreply.github.com>
2023-03-22 23:48:31 -0600

    Add files via upload

---
+++ Practica1603/insertarEstudiante.php
@@ -0,0 +1,19 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    require_once 'conexion.php';
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $carrera = $_POST["carrera"];
+    $año = $_POST["año"];
+    $my_query = "insert into estudiante(nombre, apellido, carrera, año) 
+    values('".$nombre."', '".$apellido."','".$carrera."', '".$año."')";
+    $result = $mysql -> query($my_query);
+    if($result == true){
+        echo "Registro guardado satisfactoriamente...";
+    }else{
+echo "Error al guardar registro...";
+    }
+}else{
+    echo"Error desconocido";
+}
+?>
